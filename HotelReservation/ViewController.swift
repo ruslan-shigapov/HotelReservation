954314bf206a53cee1d18fad67ef0305a86e1c3d
Ruslan Shigapov <ilgamovich@gmail.com>
2023-12-14 20:39:14 +0300

    Add project

---
+++ HotelReservation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelReservation
+//
+//  Created by Ruslan Shigapov on 14.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
